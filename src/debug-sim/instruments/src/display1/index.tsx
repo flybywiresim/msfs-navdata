efbb57f0d52fb5baac3ab7ea746ef303f81ab4b0
Alex Cutforth <alex@densem.net>
2021-08-24 09:21:26 +1200

    split debug sim panel into different pages

--- src/debug-sim/instruments/src/display1/index.tsx
+++ src/debug-sim/instruments/src/display1/index.tsx
@@ -1,102 +1,32 @@
-import React, {useEffect, useState} from 'react';
-import './index.css';
-import { render } from "../render";
-import {
-    FlightPlanProvider,
-    useCurrentOrTemporaryFlightPlan,
-    useFlightPlanManager,
-    useFlightPlans,
-    useNavDatabase
-} from "../common/flightplans";
-import {Runway} from "../../../../shared/types/Runway";
-import {Departure} from "../../../../shared/types/Departure";
-import {ProcedureTransition} from "../../../../shared/types/Common";
-import {Approach} from "../../../../shared/types/Approach";
-import {Arrival} from "../../../../shared/types/Arrival";
+import {render} from "../render";
+import {FlightPlanProvider} from "../common/flightplans";
+import React, {useState} from "react";
+import {Init} from "./init";
+import {Fpln} from "./fpln";
+import {Search} from "./search";
 
-export const DebugDisplay1 = () => {
-    const fpm = useFlightPlanManager();
-    const database = useNavDatabase();
-    const [flightPlan] = useCurrentOrTemporaryFlightPlan();
-
-    const [originRunways, setOriginRunways] = useState<Runway[]>();
-    const [originDepartures, setOriginDepartures] = useState<Departure[]>();
-    const [departureTransitions, setDepartureTransitions] = useState<ProcedureTransition[]>();
-
-    const [arrivalTransitions, setArrivalTransitions] = useState<ProcedureTransition[]>();
-    const [arrivals, setArrivals] = useState<Arrival[]>();
-    const [approachTransitions, setApproachTransitions] = useState<ProcedureTransition[]>();
-    const [approaches, setApproaches] = useState<Approach[]>();
-
-    useEffect(() => {
-        if(flightPlan.originAirport) {
-            database.getRunways(flightPlan.originAirport.ident).then(r => setOriginRunways(r));
-            database.getDepartures(flightPlan.originAirport.ident).then(r => setOriginDepartures(r));
+const DebugDisplay1 = () => {
+    const [page, setPage] = useState(0);
+    const content = () => {
+        switch(page) {
+            case(0):
+                return <Init/>
+            case(1):
+                return <Fpln/>
+            case(2):
+                return <Search/>
         }
-    }, [flightPlan.originAirport]);
-    useEffect(() => {
-        if(flightPlan.originAirport) {
-            database.getDepartures(flightPlan.originAirport.ident).then(r => setDepartureTransitions(r.find(dep => dep.ident === flightPlan.procedureDetails.departureIdentifier)?.enrouteTransitions));
-        }
-    }, [flightPlan.procedureDetails.departureIdentifier]);
-
-    useEffect(() => {
-        if(flightPlan.destinationAirport) {
-            database.getArrivals(flightPlan.destinationAirport.ident).then(r => setArrivals(r));
-            database.getApproaches(flightPlan.destinationAirport.ident).then(r => setApproaches(r));
-        }
-    }, [flightPlan.destinationAirport]);
-
-    useEffect(() => {
-        if(flightPlan.destinationAirport) {
-            database.getArrivals(flightPlan.destinationAirport.ident).then(r => setArrivalTransitions(r.find(dep => dep.ident === flightPlan.procedureDetails.arrivalIdentifier)?.enrouteTransitions));
-        }
-    }, [flightPlan.procedureDetails.arrivalIdentifier]);
-
-    useEffect(() => {
-        if(flightPlan.destinationAirport) {
-            database.getApproaches(flightPlan.destinationAirport.ident).then(r => setApproachTransitions(r.find(dep => dep.ident === flightPlan.procedureDetails.approachIdentifier)?.transitions));
-        }
-    }, [flightPlan.procedureDetails.approachIdentifier]);
-
+    }
     return(
-        <div style={{ backgroundColor: "white" }}>
-            <h2>Origin: {flightPlan.originAirport?.ident}, Runway: {flightPlan.procedureDetails.departureRunwayIdentifier}, Departure: {flightPlan.procedureDetails.departureIdentifier}, Transition: {flightPlan.procedureDetails.departureTransitionIdentifier} </h2>
-            <input onChange={(e) => {
-                if(e.target.value.length === 4)
-                    fpm.setOrigin(e.target.value);
-            }} />
+        <div>
             <select>
-                {originRunways?.map(runway => <option onClick={() => fpm.setDepartureRunwayIdentifier(runway.ident)}>{runway.ident}</option>)}
+                <option onClick={() => setPage(0)}>INIT</option>
+                <option onClick={() => setPage(1)}>F-PLN</option>
+                <option onClick={() => setPage(2)}>SEARCH</option>
             </select>
-            <select>
-                {originDepartures?.map(departure => <option onClick={() => fpm.setDepartureIdentifier(departure.ident)}>{departure.ident}</option>)}
-            </select>
-            <select>
-                {departureTransitions?.map(trans => <option onClick={() => fpm.setDepartureTransitionIdentifier(trans.ident)}>{trans.ident}</option>)}
-            </select>
-
-            <h2>To: {flightPlan.destinationAirport?.ident}, Transition: {flightPlan.procedureDetails.arrivalTransitionIdentifier}, Arrival: {flightPlan.procedureDetails.arrivalIdentifier}</h2>
-            <input onChange={(e) => {
-                if(e.target.value.length === 4)
-                    fpm.setDestination(e.target.value);
-            }} />
-
-            <select>
-                {arrivalTransitions?.map(trans => <option onClick={() => fpm.setArrivalTransitionIdentifier(trans.ident)}>{trans.ident}</option>)}
-            </select>
-            <select>
-                {arrivals?.map(arrivals => <option onClick={() => fpm.setArrivalIdentifier(arrivals.ident)}>{arrivals.ident}</option>)}
-            </select>
-            <select>
-                {approachTransitions?.map(trans => <option onClick={() => fpm.setApproachTransitionIdentifier(trans.ident)}>{trans.ident}</option>)}
-            </select>
-            <select>
-                {approaches?.map(approach => <option onClick={() => fpm.setApproachIdentifier(approach.ident)}>{approach.ident}</option>)}
-            </select>
-
-            {flightPlan.legs.map(leg => <h4>{leg.identifier}</h4>)}
+            {content()}
         </div>
     );
 }
+
 render(<FlightPlanProvider><DebugDisplay1/></FlightPlanProvider>)
---
+++ src/debug-sim/instruments/src/display1/search.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Search = () => {
+    return(
+        <div>
+            <h2>Airway:</h2>
+            <input></input>
+        </div>
+    );
+}
