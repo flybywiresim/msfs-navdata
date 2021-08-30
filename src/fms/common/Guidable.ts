bc872da8af364a12287accb3664ee85f0ce06272
Alex Cutforth <alex@densem.net>
2021-08-31 08:28:47 +1200

    delete fms & debug sim, further fms development to be done on a380x

--- src/fms/common/Guidable.ts
+++
@@ -1,8 +0,0 @@
-import { GuidanceParameters } from "./ControlLaws";
-import {Degrees, Location, NauticalMiles} from "../../shared/types/Common";
-
-export interface Guidable {
-    getGuidanceParameters(ppos: Location, trueTrack: Degrees): GuidanceParameters | null;
-    getDistanceToGo(ppos: Location): NauticalMiles;
-    isAbeam(ppos: Location): boolean;
-}
--- src/fms/common/GuidanceConstants.ts
+++
@@ -1,2 +0,0 @@
-export const k2 = 0.0045;
-export const maxRollRate = 5; // deg / s
